76df0bf7631ea8279ebca6b2da75a38c0d051984
xtaieer <920557080@qq.com>
2023-09-23 22:42:56 +0800

    内存分配函数

--- kernel/source/memory.c
+++ kernel/source/memory.c
@@ -248,3 +248,95 @@ void init_memory()
 		flush_tlb();
 	}
 }
+
+/*
+
+	number: number < 64
+
+	zone_select: zone select from dma , mapped in  pagetable , unmapped in pagetable
+
+	page_flags: struct Page flages
+
+*/
+
+struct Page* alloc_pages(int zone_select, int number, unsigned long page_flags)
+{
+	int i;
+	unsigned long page = 0;
+
+	int zone_start = 0;
+	int zone_end = 0;
+
+	switch (zone_select)
+	{
+	case ZONE_DMA:
+		zone_start = 0;
+		zone_end = ZONE_DMA_INDEX;
+
+		break;
+
+	case ZONE_NORMAL:
+		zone_start = ZONE_DMA_INDEX;
+		zone_end = ZONE_NORMAL_INDEX;
+
+		break;
+
+	case ZONE_UNMAPED:
+		zone_start = ZONE_UNMAPED_INDEX;
+		zone_end = memory_management_struct.zones_size - 1;
+
+		break;
+
+	default:
+		color_printk(RED, BLACK, "alloc_pages error zone_select index\n");
+		return NULL;
+		break;
+	}
+
+	for (i = zone_start; i <= zone_end; i++)
+	{
+		struct Zone* z;
+		unsigned long j;
+		unsigned long start, end, length;
+		unsigned long tmp;
+
+		if ((memory_management_struct.zones_struct + i)->page_free_count < number)
+			continue;
+
+		z = memory_management_struct.zones_struct + i;
+		// 把地址等数据映射成page的索引
+		start = z->zone_start_address >> PAGE_SHIFT;
+		end = z->zone_end_address >> PAGE_SHIFT;
+		length = z->zone_length >> PAGE_SHIFT;
+
+		tmp = 64 - start % 64;
+
+		for (j = start; j <= end; j += j % 64 ? tmp : 64)
+		{
+			unsigned long* p = memory_management_struct.bits_map + (j >> 6);
+			unsigned long shift = j % 64;
+			unsigned long k;
+			for (k = shift; k < 64 - shift; k++)
+			{
+				if (!(((*p >> k) | (*(p + 1) << (64 - k))) & (number == 64 ? 0xffffffffffffffffUL : ((1UL << number) - 1))))
+				{
+					unsigned long	l;
+					page = j + k - 1;
+					for (l = 0; l < number; l++)
+					{
+						struct Page* x = memory_management_struct.pages_struct + page + l;
+						page_init(x, page_flags);
+					}
+					goto find_free_pages;
+				}
+			}
+
+		}
+	}
+
+	return NULL;
+
+find_free_pages:
+
+	return (struct Page*)(memory_management_struct.pages_struct + page);
+}
